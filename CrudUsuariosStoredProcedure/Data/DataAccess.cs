d5321afe1ebd4a7ab3da4cb774b8ebbb8b2ac59c
Bruno Ferreira da Silva <brunosilva@polimold.com.br>
2024-09-25 23:01:12 -0300

    Adicionar arquivos de projeto.

---
+++ CrudUsuariosStoredProcedure/Data/DataAccess.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.SqlClient;
+
+namespace CrudUsuariosStoredProcedure.Data
+{
+    public class DataAccess
+    {
+        SqlConnection _connection = null;
+        SqlCommand _command = null;
+        public static IConfiguration Configuration { get; set; }
+
+        private string GetConnectionString()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsetings.json");
+
+            Configuration = builder.Build();
+
+            return Configuration.GetConnectionString("DefaultConnection");
+        }
+        
+    }
+}
